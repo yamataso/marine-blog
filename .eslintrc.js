e8bde07646edc77e28f55f3d0d9fadc86776bcc9
yamataso <yamataso0876@gmail.com>
2022-04-03 22:14:00 +0900

    ✨ Tailwindcss,ESLint,Pretterの実装

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    quotes: "error",
+  },
+};
