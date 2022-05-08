900281cc633a964d1e8860cfd3df5a7b7fc2bee9
yamataso <yamataso0876@gmail.com>
2022-05-08 09:15:16 +0900

    🔥

--- src/libs/client.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "microcms-js-sdk"; //ES6
-
-// Initialize Client SDK.
-export const client = createClient({
-  serviceDomain: "marine-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.API_KEY,
-});
