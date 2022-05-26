ef4f3575642a736aea3d12fd7b47a10d8eb7738b
yamataso <yamataso0876@gmail.com>
2022-05-26 23:24:36 +0900

    ♻️ ホームを修正しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ export type Blog = {
 type Props = MicroCMSListResponse<Blog>;
 const Home: NextPage<Props> = (props) => {
   const [search, setSeach] = useState<MicroCMSListResponse<Blog>>();
-
   const hundleSubmit: ComponentProps<"form">["onSubmit"] = async (event) => {
     event.preventDefault();
     const q = event.currentTarget.query.value;
@@ -35,25 +34,6 @@ const Home: NextPage<Props> = (props) => {
 
   return (
     <div>
-      <form className="flex gap-x-2 my-5" onSubmit={hundleSubmit}>
-        <input
-          type="text"
-          name="query"
-          className="border-2 shadow-md px-2 border-black"
-        />
-        <button className="px-2 ml-3 border-2 border-black">検索</button>
-        <button
-          type="reset"
-          className="px-2 ml-3 border-2 border-black"
-          onClick={handleClick}
-        >
-          リセット
-        </button>
-      </form>
-      <h1 className=" py-2 text-center  bg-slate-200">
-        ~明日を生き抜く力を与える~
-      </h1>
-
       <div>
         <nav
           className="relative flex  sm:h-10 lg:justify-start"
@@ -61,31 +41,79 @@ const Home: NextPage<Props> = (props) => {
         ></nav>
       </div>
       <img
-        className="  bg-gradient-to-r md:bg-gradient-to-t  left-0  "
+        className="m-auto rounded-lg "
         src="/220_M.jpg"
-        alt=""
+        alt="ヒーロー画像"
+        width={1024}
+        height={980}
       />
-      <div>
-        <p className=" text-gray-400">{`${
-          search ? "検索結果" : "記事の総数"
-        }${totalCount}件`}</p>
+      <p className=" text-gray-400 px-44 mt-16">{`${
+        search ? "検索結果" : "記事の総数"
+      }${totalCount}件`}</p>
 
+      <div className="flex  m-auto">
         <ul className="mt-4 space-y-4">
           {contents.map((content) => {
             return (
-              <li key={content.id}>
+              <li key={content.id} className="min-w-max pl-44">
                 <Link href={`/blog/${content.id}`}>
-                  <a
-                    href="/"
-                    className=" underline text-blue-800 hover:text-blue-400"
-                  >
-                    {content.title}
-                  </a>
+                  <div className="flex flex-col md:flex-row items-center border rounded-lg overflow-hidden p-3">
+                    <img
+                      src="/profile.png"
+                      loading="lazy"
+                      alt="画像データ"
+                      className="w-72  h-36 rounded-lg object-cover object-center  inset-0 group-hover:scale-110 transition duration-200"
+                    />
+
+                    <div className="m-4 p-4">
+                      <h2 className="text-3xl">
+                        <a
+                          href="/"
+                          className=" underlinegroup w-full md:w-24  lg:w-auto h-24 md:h-full block self-start shrink-0 text-indigo-500 hover:text-indigo-600 active:text-indigo-700overflow-hidden relative"
+                        >
+                          {content.title}
+                        </a>
+                      </h2>
+                    </div>
+                    <div className="pt-20 pl-4 text-right">
+                      <span className="text-gray-400 text-sm pr-3">
+                        2022/05/21
+                      </span>
+                    </div>
+                  </div>
                 </Link>
               </li>
             );
           })}
         </ul>
+        <div className="mx-4">
+          <form className="text-right space-x-4" onSubmit={hundleSubmit}>
+            <input
+              type="text"
+              name="query"
+              placeholder="キーワード検索"
+              className="px-2 py-1 border-2 shadow-md  border-black rounded-lg "
+            />
+            <button className="px-5 mt-3 border-2 border-black rounded-lg">
+              <img
+                src="/magnifying-glass.png"
+                alt="検索虫メガネ"
+                width={24}
+                height={24}
+              />
+            </button>
+            <button
+              type="reset"
+              className="px-1 my-2 border-2 border-black rounded-lg "
+              onClick={handleClick}
+            >
+              <img src="/reset.png" alt="リセット画像" width={24} height={24} />
+            </button>
+          </form>
+          <div className="min-w-max mt-4 ml-12 p-5 flex w-70 h-60 flex-col md:flex-row items-center border rounded-lg overflow-hidden">
+            <p>{props.category && `${props.category.name}`}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
