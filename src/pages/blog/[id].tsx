80f9746c62be1e2ccc51b17e2856c818a924425c
yamataso <yamataso0876@gmail.com>
2022-05-28 15:55:25 +0900

    ✨ 詳細ページにプロフィール欄を作成しました

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -4,24 +4,38 @@ import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Blog } from "..";
 import { client } from "../../libs/client";
+import { ProfileJP } from "../../../components/ProfileJP";
+import { ProfileYama } from "../../../components/ProfileYama";
 
 type Props = Blog & MicroCMSContentId & MicroCMSDate;
 
 const BlogId: NextPage<Props> = (props) => {
   return (
-    <div className="ml-40 pl-4">
-      <h1 className=" py-4 text-4xl font-bold">{props.title}</h1>
+    <div className="ml-40 pl-4 ">
+      <div className="min-w-max">
+        <h1 className=" py-4 text-4xl font-bold">{props.title}</h1>
 
-      <time dateTime={props.publishedAt} className="my-2 py-2 block">
-        {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
-      </time>
-      <hr className="my-10 mr-20" />
-      {/* <p>{props.category && `${props.category.name}`}</p> */}
+        <time dateTime={props.publishedAt} className="my-2 py-2 block">
+          {dayjs(props.publishedAt).format("YYYY年MM月DD日")}
+        </time>
+        <hr className="my-10 mr-20" />
+        {/* <p>{props.category && `${props.category.name}`}</p> */}
 
-      <article
-        className=" prose  prose-headings:container "
-        dangerouslySetInnerHTML={{ __html: props.body }}
-      />
+        <div className=" flex">
+          <article
+            className=" prose  prose-headings:container  m-4 p-2"
+            dangerouslySetInnerHTML={{ __html: props.body }}
+          />
+          <div className="ml-10 pl-5">
+            <div className="m-5 mt-4 ml-12 p-2 flex  w-80 h-96  md:flex-row items-top border rounded-lg overflow-hidden">
+              <ProfileJP />
+            </div>
+            <div className="m-5 mt-4 ml-12 p-2 flex  w-80 h-96  md:flex-row items-top border rounded-lg overflow-hidden">
+              <ProfileYama />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
