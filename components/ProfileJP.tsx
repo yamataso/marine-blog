a8a9266afaf2f7e3b43124057f3ba02d074329d1
yamataso <yamataso0876@gmail.com>
2022-06-04 09:07:12 +0900

    📸 画像を変更しました。

--- components/ProfileJP.tsx
+++ components/ProfileJP.tsx
@@ -2,24 +2,23 @@ import React from "react";
 
 export const ProfileJP = () => {
   return (
-    <div className="">
+    <div>
       <h1 className="font-semibold pb-4">プロフィール</h1>
       <img
         className="ml-2 rounded-full"
-        src="/profile.png"
+        src="/JP.png"
         alt="プロフィール画像JP"
         width={100}
         height={100}
       />
-      <div className="m-1 ">
-        <p className="font-extrabold">JP</p>
-        <p className="m-2 pt-1 font-medium">
-          【理学療法士✖スポーツ栄養プランナー】20代社会人。
-          <br />
-          動く・食べる・寝るが人のパフォーマンスを最高に高める事を証明するためにブログ発信しています(
-          ^ω^ )
-        </p>
-      </div>
+
+      <p className="font-extrabold">JP</p>
+      <p className="m-2 pt-1 font-medium overflow-hidden">
+        【理学療法士✖スポーツ栄養プランナー】20代社会人。
+        <br />
+        動く・食べる・寝るが人のパフォーマンスを最高に高める事を証明するためにブログ発信しています(
+        ^ω^ )
+      </p>
     </div>
   );
 };
