06dfeacd0632ccdbae7511a7b583f90084937722
yamataso <yamataso0876@gmail.com>
2022-06-11 01:17:23 +0900

    ♻️ 著作権の表示を変更しました。

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="mt-16  bg-gray-900">
       <div className="text-gray-400 text-sm text-center border-t border-gray-800 py-8">
-        © 2021 - Present Flowrift. All rights reserved.
+        © 2022 - Marine-blog. All rights reserved.
       </div>
     </footer>
   );
