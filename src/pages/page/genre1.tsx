9db6dabbc37f61cd8714d7cdcb9b347984d5472b
yamataso <yamataso0876@gmail.com>
2022-06-11 00:47:03 +0900

    🔥

--- src/pages/page/genre1.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Genre1 = () => {
-  return (
-    <div>
-      <h1>genre1</h1>
-    </div>
-  );
-};
-export default Genre1;
--- src/pages/page/genre2.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Genre2 = () => {
-  return (
-    <div>
-      <h1>genre2</h1>
-    </div>
-  );
-};
-export default Genre2;
