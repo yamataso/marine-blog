53bbeaafb075b622d68dcd52ca13876d68daff1c
yamataso <yamataso0876@gmail.com>
2022-07-03 13:49:11 +0900

    ✨ google AdSenseに対応しました。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../../components/Layout/Layout";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3288980965726316"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
